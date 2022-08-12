5508eb4e4cfc4a367d4d382698ef6d5e70e368c1
Marcelo Silveira <marcelosj3@hotmail.com>
2022-08-12 13:26:50 -0300

    feat: dynamic sort function

--- src/services/rank.service.ts
+++ src/services/rank.service.ts
@@ -13,12 +13,33 @@ class RankService {
 
   getRankInfo = async <T>(
     mapCallback: IMapCallback<T>,
-    order: TOrderBy<T>
+    orderBy: TOrderBy<T>
     // query: ParsedQs
   ) => {
     const playerMatches: PlayerMatch[] = await PlayerMatchRepository.findAll();
 
     const rankInfo = playerMatches.map(mapCallback);
+
+    rankInfo.sort((playerA, playerB) => {
+      const orders = Object.entries(orderBy);
+
+      for (const order of orders) {
+        const [key, value] = order;
+
+        const sort = value === "ASC" ? 1 : value === "DESC" ? -1 : 0;
+        const invertedSort = value === "DESC" ? 1 : value === "ASC" ? -1 : 0;
+
+        const pA = playerA[key as keyof T];
+        const pB = playerB[key as keyof T];
+
+        if (value) {
+          if (pA > pB) return sort;
+          if (pA < pB) return invertedSort;
+        }
+      }
+
+      return 0;
+    });
 
     return rankInfo;
   };
@@ -26,8 +47,7 @@ class RankService {
   getKills = async ({ query }: Request) => {
     const includeMatchUrl = query.hasOwnProperty("match_url");
 
-    const order: TOrderBy<IRanksKills> = { kills: "DESC", name: undefined };
-
+    const order: TOrderBy<IRanksKills> = { kills: "DESC", name: "DESC" };
     const playersInfo: IMapCallback<IRanksKills> = (playerMatch) => ({
       name: playerMatch.player.name,
       kills: playerMatch.kills,
